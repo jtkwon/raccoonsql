89325a1461a761c878a0246a7c3efff5d1cea047
jtkwon <jeff.t.kwon@gmail.com>
2023-04-15 22:30:54 -0400

    updated mod

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jtkwon/raccoonsql
 
 go 1.20
+
+replace github.com/cube2222/octosql v0.0.0 => "./octosql"
---
+++ src/golib/lib.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/jtkwon/raccoonsql/octosql/parser"
+)
+
+import "C"
+
+func test() {
+	parser.ParseSelect("")
+}
+
+func main() {}
